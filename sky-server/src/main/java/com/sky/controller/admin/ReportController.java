73a11637ceedeb8dd691ca0da582cd01475f10f4
zerd1y <xiaojinghuai33@gmail.com>
2025-06-27 23:11:20 +0800

    add "export operation data reports" code

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 @RestController
@@ -82,6 +83,13 @@ public class ReportController {
     ) {
         log.info("销量排名top10: {}, {}", begin, end);
         return Result.success(reportService.getSalesTop10(begin, end));
+    }
+
+    //导出运营数据报表
+    @GetMapping("/export")
+    @ApiOperation("导出运营数据报表")
+    public void export(HttpServletResponse response){
+        reportService.exportBusinessData(response);
     }
 
 }
--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -6,6 +6,7 @@ import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 @Service
@@ -40,4 +41,10 @@ public interface ReportService {
      * @return
      */
     SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end);
+
+    /**
+     * 导出运营数据表
+     * @param response
+     */
+    void exportBusinessData(HttpServletResponse response);
 }
